35a949a40e33f250395e9c9ba24e12ef64d9087b
Kim, Hyeonsung <qwqwhsnote@gm.gist.ac.kr>
2021-12-28 22:59:14 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,12 @@ numfig = True
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "logo_design.gif"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
