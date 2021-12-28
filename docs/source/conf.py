c6c70e5b809a914ada29a222afb3d52b47c6fa27
Kim, Hyeonsung <qwqwhsnote@gm.gist.ac.kr>
2021-12-28 23:57:18 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ numfig = True
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "logo_design.gif"
+html_logo = "_static/logo_design.gif"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
