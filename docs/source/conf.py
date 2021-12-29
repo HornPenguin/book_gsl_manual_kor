7f23ec40974311ef0168ea654e391ce568aa39d7
Kim, Hyeonsung <qwqwhsnote@gm.gist.ac.kr>
2021-12-29 21:08:20 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ latex_elements = {
     'preamble': r'''
 \usepackage{pdfpages}
 \begin{document}
-\includepdf[pages=1]{source/_static/Cover.pdf}
+\includepdf[pages=1]{Cover.pdf}
     ''',
     'releasename': ''
 }
