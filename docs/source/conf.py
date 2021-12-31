8da4a91b867fbca23ff7df7dcc253b1f9f09e240
Raspbie <34023196+Raspbie@users.noreply.github.com>
2021-12-31 11:55:09 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,28 +81,33 @@ my_font_setting =r'''
 my_cover_design = r'''
     \newpage
     \pagecolor{coverbackground}\afterpage{\nopagecolor}
-
+    
     \begin{tikzpicture}[remember picture,overlay,shift={(current page.north west)}]
         \fill[coverband,yshift=-120mm] rectangle(\paperwidth,85mm);
     \end{tikzpicture}
 
-    \vspace{0.5cm}\hspace{4.3cm}\includegraphics[width=0.5\textwidth]{A_Slick_GNU_Logo.png}
+    \vspace{-0.5cm}\hspace{2.5cm}\includesvg[width=0.6\textwidth]{A_Slick_GNU_Logo.svg}
 
-    \begin{textblock*}{5cm}(15cm,6.4cm) % {block width} (coords) 
-        \raggedleft \huge\textbf{과학계산}
-    \end{textblock*}
+    \begin{textblock*}{4.25cm}(15cm,5.4cm) % {block width} (coords) 
+            \raggedright{
+            \Huge\textbf{과학계산}\\
+            \Huge\textbf{라이브러리}
+            }\\
+            \raggedleft\Large\textbf{v2.7}
+        \end{textblock*}
 
-    \begin{textblock*}{10cm}(10cm,7.8cm)
-        \raggedleft\Huge\textbf{라이브러리}\\
-        \fontsize{35}{60}\selectfont \textbf{사용 설명서}
+        \begin{textblock*}{10cm}(9.1cm,9.3cm)
+            \raggedleft
+            \fontsize{32}{60}\selectfont \textbf{사용 설명서}
     \end{textblock*}
 
     \begin{center}
-    \vfill
-    \large \textbf{Mark Galassi et al} \\
-    역: 김현성 \\
-    \vspace{1.2cm}
-    \Large \textbf{GNU Publication}
+        \vfill
+        \large \textbf{Mark Galassi \\ et al} \\
+        \vspace{0.5cm}
+        역: 김현성 \\
+        \vspace{1.2cm}
+        \Large \textbf{GNU Publication}
     \end{center}
 
     \sphinxmaketitle
