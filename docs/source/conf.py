68ac6b3391a015a3768f1889a8ce5b9ff2b1d963
Kim, Hyeonsung <qwqwhsnote@gm.gist.ac.kr>
2022-01-23 14:17:39 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ my_cover_and_license = r'''
     '''
 latex_elements = {
     'papersize': 'a4paper',
-    'pointsize': '10pt',
+    'pointsize': '9pt',
     'preamble': my_preamble,
     'maketitle' : maketitle,
     'fontpkg' : fontpkg,
